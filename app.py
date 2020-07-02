77918b6f80baaa40b97e774325b14a4672626626
clayajohnson <johnson.clayton.a@gmail.com>
2020-07-03 05:07:27 +1000

    renders page from html

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello World!'
+    return render_template('index.html')
